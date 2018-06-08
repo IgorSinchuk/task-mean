6421a100af10f0401785b66bff8c0599d58520fc
Igor <igor@Igors-iMac.local>
2018-06-08 19:13:26 +0300

    login addded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: '/login',
     pathMatch: 'full'
   },
   {
