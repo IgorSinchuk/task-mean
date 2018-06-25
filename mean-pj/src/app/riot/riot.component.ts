f7eddab7e1f54ad45c712c6e17314b00f3b78866
Igor <igor@Igors-iMac.local>
2018-06-25 16:38:07 +0300

    riot component fixed

--- mean-pj/src/app/riot/riot.component.ts
+++ mean-pj/src/app/riot/riot.component.ts
@@ -5,19 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './riot.component.html',
   styleUrls: ['./riot.component.css']
 })
-
-
 export class RiotComponent implements OnInit {
 
-public apiKey: "";
-public URL: "";
-
-  
   constructor() { }
-  
+
   ngOnInit() {
   }
 
 }
-
-
